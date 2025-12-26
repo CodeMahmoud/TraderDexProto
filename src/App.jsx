36f985ca62309f9b700abafdc43d03903b845d6a
CodeMahmoud <mahmoudcode913@gmail.com>
2025-12-25 18:19:39 -0600

    updated App file, navbar  and css styles for newly added card and container components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './App.css'
 
 import Navbar from './components/Navbar';
+import CardsGrid from './components/CardsContainer/CardsContainer';
 
 function App() {
-const [data, setData] = React.useState([]);
-useEffect(() => {
-  async function fetchEffect() {
-    const response = await fetch('./public/pokeCardsAPI.json');
-    const dataJson = await response.json();
-    setData(dataJson);
-  }
-  fetchEffect();
-}, [])
-console.log(data)
   return (
     <>
-    <Navbar />
     <h1>Poke Cards</h1>
-    <div>
-      {data.map((item) => (
-        <img key={item.id} src={item.image}/>
-      ))}
-    </div>
+    <Navbar />
+    <CardsGrid />
     </>
   )
 }
